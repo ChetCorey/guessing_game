95ba0f998311d34f5202966d4affd5cf10365a5c
Chet Corey <chetcorey@hotmail.com>
2014-11-22 12:04:15 -0500

    refactoring ui

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,36 +1,68 @@
 # This is a Number guessing game
-class Guessing
-  def computer_num
-    @random_number = rand(100) + 1
-    puts 'I am thinking of a number between 1 and 100.
-          So whats you first guess?'
+require 'active_support/inflector'
+
+class GuessingGame
+  def initialize
     @count = 0
+    @computer_guess = rand(1..100)
+    puts "I am thinking of a number between 1 and 100 \nSo whats your first guess?"
   end
 
-  def random_game
-    @random_guess = gets
-    @count = @count + 1
-    if @random_number == @random_guess.to_i
-      puts 'You got it in ' + @count.to_s + ' guesses.'
-    elsif @count == 5
-      puts 'GAME OVER
-      The answer was ' + @random_number.to_s
-    elsif @random_guess.to_i > @random_number
-      puts 'Lets try again your guess too big.
-      You have ' + (5 - @count).to_s + ' guesses left.'
+  def round
+    @player_guess = gets.chomp.to_i
+    @count += 1
+    win if @player_guess == @computer_guess
+    lose if @count == 5
+    if @player_guess > @computer_guess
+      count
+      guesses_left
       random_game
     else
-      puts 'Lets try again your guess too small.
-      You have ' + (5 - @count).to_s + ' guesses left.'
+      count
+      puts 'Lets try again your guess too small.'
+      guesses_left
       random_game
     end
   end
 
+  def win
+    puts "You got it in #{@count} guesses."
+  end
+
+  def lose
+    puts "GAME OVER \nThe answer was #{@computer_guess}"
+  end
+
+  def bigger #reads "Make A GIGGER GUESS"
+    puts "Make A\n\nB\nI  /\\\nG /  \\\nR/_  _\\\nE |__|  \nR\n\nGUESS"
+  end
+
+  def smaller #reads "make a smaller guess"
+    puts "make a\ns  ||\nm  ||\na  ||\nl  ||\nl  ||\ne \\  /\nr  \\/\nguess"
+  end
+
+  def guesses_left
+    puts "You have #{5 - @count} #{'guess'.pluralize(5 - @count)} left."
+  end
+
+  def count
+    @count += 1
+  end
+
   def game
     computer_num
-    random_game
+    round
+  end
+
+  def plauralize(word, count)
+    word.to_s
+    if count == 1
+      word.pluralize(1)
+    elsif count > 1
+      word.pluralize
+    end
   end
 end
 
-play = Guessing.new
+play = GuessingGame.new
 play.game
