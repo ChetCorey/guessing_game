dc0b6054a58d07b6a1cb904a93726cec05339006
Chet Corey <chetcorey@hotmail.com>
2014-11-23 18:50:35 -0500

    upgrading

--- guessing_game.rb
+++ guessing_game.rb
@@ -5,64 +5,58 @@ class GuessingGame
   def initialize
     @count = 0
     @computer_guess = rand(1..100)
-    puts "I am thinking of a number between 1 and 100 \nSo whats your first guess?"
   end
 
   def round
-    @player_guess = gets.chomp.to_i
-    @count += 1
-    win if @player_guess == @computer_guess
-    lose if @count == 5
-    if @player_guess > @computer_guess
-      count
-      guesses_left
-      random_game
-    else
-      count
-      puts 'Lets try again your guess too small.'
-      guesses_left
-      random_game
+    puts "I am thinking of a number between 1 and 100 \nYou have get 5 guesses. Whats your first guess?" if @count == 0
+    human_guess = gets.chomp.to_i
+    outcome(human_guess, @computer_guess)
+    guesses_remaining
+  end
+
+  def play_again
+    puts "would you like to play again? (y/n)"
+    responce = gets.chomp
+    if responce == 'y'
+      initialize
+    elsif responce == 'n'
+      puts "thanks for playing"
+      exit
     end
   end
 
   def win
     puts "You got it in #{@count} guesses."
+    play_again
   end
 
   def lose
-    puts "GAME OVER \nThe answer was #{@computer_guess}"
+    puts "GAME OVER\nThe answer was : #{@computer_guess}"
+    play_again
   end
 
-  def bigger #reads "Make A GIGGER GUESS"
-    puts "Make A\n\nB\nI  /\\\nG /  \\\nR/_  _\\\nE |__|  \nR\n\nGUESS"
-  end
-
-  def smaller #reads "make a smaller guess"
-    puts "make a\ns  ||\nm  ||\na  ||\nl  ||\nl  ||\ne \\  /\nr  \\/\nguess"
-  end
-
-  def guesses_left
-    puts "You have #{5 - @count} #{'guess'.pluralize(5 - @count)} left."
-  end
-
-  def count
-    @count += 1
-  end
-
-  def game
-    computer_num
-    round
-  end
-
-  def plauralize(word, count)
-    word.to_s
-    if count == 1
-      word.pluralize(1)
-    elsif count > 1
-      word.pluralize
+  def outcome(human_guess, computer_guess) #directions are smaller or bigger
+    if computer_guess == human_guess
+      win && play_again
+    elsif @count >= 5
+      lose
+    elsif computer_guess < human_guess # smaller
+      puts "make a\n    __\ns  |  |\nm  |  |\na  |  |\nl__|  |__\nl\\      /\ne \\    /\nr  \\  /\n    \\/\nguess"
+    elsif computer_guess > human_guess # bigger
+      puts "Make A\n    /\\\nB  /  \\\nI /    \\\nG/__  __\\\nR  |  |\nE  |  |\nR  |  |\n   |__|\nGUESS"
+    else
+      puts "You made an illegal move, guess again."
+      @count -= 1
+      round
     end
+  end
+
+  def guesses_remaining
+    puts "You have #{5 - @count} #{'guess'.pluralize(5 - @count)} left." if @count < 5
+    @count += 1
+    round
   end
 end
 
 play = GuessingGame.new
-play.game
+play.round
